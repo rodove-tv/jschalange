e97757bd58628e490ed3e2ee1b1b6ac44ebc3e6b
Coco-Bd <146067674+Coco-Bd@users.noreply.github.com>
2024-04-23 14:51:07 +0200

    snake game transfer from old repository

--- snake.js
+++ snake.js
@@ -1,61 +1,130 @@
-const container = document.getElementById("game");
-const canva = document.createElement("div");
-const border = document.createElement("div");
-container.appendChild(border);
-border.appendChild(canva);
-
-/*------------------------style------------------------*/
-border.style.aspectRatio = "4/3";
-border.style.height = "100%";
-border.style.border = "5px solid black";
-border.style.boxShadow = "inner 0 0 10px black";
-canva.style.backgroundColor = "grey";
-canva.style.display = "grid";
-canva.style.width = "100%";
-canva.style.height = "100%";
-canva.style.gridTemplateColumns = "repeat(20, 1fr)";
-canva.style.gridTemplateRows = "repeat(20, 1fr)";
-
-/*------------------------snake------------------------*/
-
-const snake = [(x = 10), (y = 10)];
-let direction = "right";
+const gameboard = document.getElementById("game-board");
+let food = addFood();
+let snake = [{ x: 10, y: 10 }];
+let direcrtion = "up";
+let gameStatus;
+let gameInterval;
+let interval = 500;
 let score = 0;
-let speed = 200;
-let gameover = false;
+let hightScore = 0;
 
-function draw() {
-  canva.innerHTML = ""; //reset board
-  drawSnake();
-}
+// event listener for key press
+document.addEventListener("keydown", (event) => {
+  switch (event.key) {
+    case "ArrowUp":
+      direcrtion = "up";
+      break;
+    case "ArrowDown":
+      direcrtion = "down";
+      break;
+    case "ArrowLeft":
+      direcrtion = "left";
+      break;
+    case "ArrowRight":
+      direcrtion = "right";
+      break;
+  }
+});
 
+/* --------------------- Draw Functions --------------------- */
+
+// draw the snake
 function drawSnake() {
-  snake.forEach(() => {
-    const snakeElement = document.createElement("div");
-    snakeElement.style.gridColumn = snake[0];
-    snakeElement.style.gridRow = snake[1];
-    snakeElement.style.backgroundColor = "black";
-    canva.appendChild(snakeElement);
+  snake.forEach((segment) => {
+    const snakeElement = createGameElement("div", "snake");
+    setElementposition(snakeElement, segment);
+    gameboard.appendChild(snakeElement);
   });
 }
 
-function move() {
-  addEventListener("keydown", (direction) => {
-    switch (direction) {
-      case "up":
-        snake[1]--;
-        break;
-      case "down":
-        snake[1]++;
-        break;
-      case "left":
-        snake[0]--;
-        break;
-      case "right":
-        snake[0]++;
-        break;
-    }
-  });
+// draw the food
+function drawFood() {
+  const foodElement = createGameElement("div", "food");
+  setElementposition(foodElement, food);
+  gameboard.appendChild(foodElement);
 }
 
-drawSnake();
+/* --------------------- Game Functions --------------------- */
+
+// main game function
+function game() {
+  drawFood();
+  drawSnake();
+  console.log("Snake.js loaded!");
+}
+
+function checkGameOver() {
+  if (snake.x < 1 || snake.x > 20 || snake.y < 1 || snake.y > 20) {
+    console.log("Game Over!");
+    gameStatus = "over";
+  }
+}
+
+function initGame() {
+  gameStatus = "running";
+  game();
+  interval = 500;
+  gameInterval = setInterval(() => {
+    moveSnake();
+    game();
+    checkGameOver();
+  }, interval);
+}
+
+// move the snake
+function moveSnake() {
+  const head = { ...snake[0] };
+  switch (direcrtion) {
+    case "left":
+      head.x--;
+      break;
+    case "up":
+      head.y--;
+      break;
+    case "right":
+      head.x++;
+      break;
+    case "down":
+      head.y++;
+      break;
+  }
+  snake.unshift(head);
+  console.log(snake.unshift(head));
+  if (head.x === food.x && head.y === food.y) {
+    food = addFood();
+    score++;
+    clearInterval();
+    interval -= 50;
+    gameInterval = setInterval(() => {
+      moveSnake();
+      game();
+      checkGameOver();
+    }, interval);
+  } else {
+    snake.pop();
+  }
+}
+
+// add food into the game
+function addFood() {
+  const x = Math.floor(Math.random() * 20) + 1;
+  const y = Math.floor(Math.random() * 20) + 1;
+  return { x, y };
+}
+
+/* --------------------- Helper Functions --------------------- */
+
+// set the position of the element
+function setElementposition(element, position) {
+  element.style.gridRowStart = position.y;
+  element.style.gridColumnStart = position.x;
+}
+
+// create elelemnt for the game into html
+function createGameElement(element, className) {
+  const gameElement = document.createElement(element);
+  gameElement.className = className;
+  return gameElement;
+}
+
+moveSnake();
