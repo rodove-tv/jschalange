3e96e93e750bd826c15cb69aa7abbd6db5fa7568
coco-bd <ccbd31@gmail.com>
2024-05-01 23:05:04 +0200

    index page game divs style + animations

---
+++ public/js/effects.js
@@ -0,0 +1,9 @@
+const Footer = Document.getElementById("footer");
+const nav = Document.getElementById("nav");
+const Container = Document.getElementById("LayoutContainer");
+
+function Effect(gameName) {
+  nav.style.display = "none";
+  Footer.style.display = "none";
+  const game = Document.getElementById(gameName);
+}
